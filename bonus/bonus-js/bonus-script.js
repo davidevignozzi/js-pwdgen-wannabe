fd5c893e55e9c5a7499463e672a26d480a989e3f
Davide <davide.vignozzi@gmail.com>
2020-12-11 15:28:12 +0100

    Finale molto triste
    In realtà volevo fare in modo che alla domanda qual è il tuo colore preferito , lo sfondo diventasse di quel colore (con un innerhtml con scritto tipo <style background: var-colore) ma non ce l'ho fatta...

--- bonus/bonus-js/bonus-script.js
+++ bonus/bonus-js/bonus-script.js
@@ -1,1 +1,21 @@
-var nome = prompt('che cazzo fai?')
+var nome = prompt('Ciao, come ti chiami?');
+console.log('Il Tipo si chiama ' + nome);
+
+var età = prompt('Ciao ' + nome + ' quanti anni hai?');
+var annoCorrente = new Date().getFullYear();
+var annoDiNascita = (annoCorrente - età);
+console.log('Il tipo è del ' + annoDiNascita);
+
+if (età < 18) {
+  alert(nome + ', questo è un sito per adulti sicuro di avere ' + età + ' anni? Secondo me ne hai qualcuno in più, Ti rifaccio la domanda')
+  var etàFalsa = prompt('Ciao ' + nome + ' quanti anni hai?');
+  var annoDiNascitaFalso = (annoCorrente - etàFalsa);
+  console.log('Ok, adesso ' + nome + ' è del ' + annoDiNascitaFalso)
+  var passwordContent = document.getElementById('password');
+  passwordContent.innerText = 'La tua nuova password super sicura è ' + nome + annoDiNascitaFalso
+}
+
+if (età > 18) {
+  var passwordContent = document.getElementById('password');
+  passwordContent.innerText = 'La tua nuova password super sicura è ' + nome + annoDiNascita
+}
