1c0df8c46d9b188b89e0537bb0039b5857e621cc
Davide <davide.vignozzi@gmail.com>
2020-12-11 14:11:07 +0100

    Correzione di grammatica che spero non troverete...

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ console.log('Il nome dell\'utente è ' + nome);
 var cognome = prompt('Inserisci qui il tuo cognome');
 console.log('Il cognome dell\'utente è ' + cognome);
 
-var colore = prompt('Qual\'è il tuo colore preferito?');
+var colore = prompt('Qual è il tuo colore preferito?');
 console.log('Il colore preferito dell\'utente è ' + colore);
 
 var annoCorrente = new Date().getFullYear();
